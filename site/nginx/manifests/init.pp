e16ee14a2946fc6270bf32b331335d44bfc17724
djs6255 <djs6255@gmail.com>
2016-03-30 08:43:44 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,5 +3,23 @@ class nginx {
   package { 'nginx':
     ensure  =>  present,
   }
+  
+  file { '/var/www/index.html':
+    ensure  => file,
+    source  => 'puppet:///site/nginx/files/index.html',
+    require =>  'Package['nginx'],
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    source  => 'puppet:///site/nginx/files/nginx.conf',
+    require => Package['nginx'],
+  }
+  
+  file { '/etc/nginx/conf.d/defalt.conf':
+    ensure  => file,
+    source  =>  'puppet:///site/nginx/files/default.conf',
+    require =>  Package['nginx'],
+  }
 
 }
