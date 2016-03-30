41d3a9e6f8e9a5ef0b3eb7d15e73965fe80c4924
djs6255 <djs6255@gmail.com>
2016-03-30 07:44:14 -0700

    Removed users class

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  class { 'users': }
   class { 'skeleton': }
   
   notify { "Hello, my name is ${::hostname}": }
