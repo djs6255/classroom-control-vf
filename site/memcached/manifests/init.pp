a1df185b1fb8cf40f601994f57c77f57559863a3
djs6255 <djs6255@gmail.com>
2016-03-30 08:15:38 -0700

    Char issue

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,15 +1,15 @@
 class memcached {
-  package ( 'memcached' :
+  package ( 'memcached':
     ensure  =>  present,
   }
   
-  file ( '/etc/sysconfig/memcached' :
+  file ( '/etc/sysconfig/memcached':
     ensure  =>  file,
     content =>  'PORT="11211"\nUSER="memcached"\nMAXCONN="96"\nCACHESIZE="32"\nOptions=""',
     require =>  Package['memcached'],
   }
   
-  service { 'memcached' :
+  service { 'memcached':
     ensure  =>  running,
     require =>  File['/etc/sysconfig/memcached',Package['memcached']],
   }
