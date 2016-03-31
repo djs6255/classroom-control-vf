0871d89c86ff547c300a9b3a082d7ef5af26c539
djs6255 <djs6255@gmail.com>
2016-03-31 10:10:03 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,42 +1,6 @@
 class nginx ( String $root = '/var/www' ) {
+  include nginx::params
 
-  case $osfamily {
-    'RedHat':   {
-      $package = 'nginx'
-      $fowner = 'root'
-      $fgroup = 'root'
-      $docroot = "$root"
-      $confdir = '/etc/nginx'
-      $srvblkdir = "${confdir}/conf.d"
-      $logsdir = '/var/log/nginx'
-      $srvname = 'nginx'
-      $srvuser = 'nginx'
-    }
-    'Debian':   {
-      $package = 'nginx'
-      $fowner = 'root'
-      $fgroup = 'root'
-      $docroot = "$root"
-      $confdir = '/etc/nginx'
-      $srvblkdir = "${confdir}/conf.d"
-      $logsdir = '/var/log/nginx'
-      $srvname = 'nginx'
-      $srvuser = 'www-data'
-    }
-    'Windows':  {
-      $package = 'nginx-service'
-      $fowner = 'Administrator'
-      $fgroup = 'Administrator'
-      $confdir = 'C:/ProgramData/nginx'
-      $docroot = "$root"
-      $srvblkdir = "${confdir}/conf.d"
-      $logsdir = '${confdir}/logs'
-      $srvname = 'nginx'
-      $srvuser = 'nobody'
-    }
-    default:    { fail("OS Family ${osfamily} is not supported") }
-  }
-  
   File {
     owner =>  "${fowner}",
     group =>  "${fgroup}",
