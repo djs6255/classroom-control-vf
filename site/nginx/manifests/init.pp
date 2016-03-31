92fd7fc94d4df5465ee3dd23c858f0234255f434
djs6255 <djs6255@gmail.com>
2016-03-31 08:30:51 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,42 +2,80 @@ class nginx {
   $wwwdir = "/var/www"
   $nginxpath = "/etc/nginx"
   
+  case $osfamily {
+    'RedHat':   {
+      $package = 'nginx',
+      $fowner = 'root',
+      $fgroup = 'root',
+      $docroot = '/var/www',
+      $confdir = '/etc/nginx',
+      $srvblkdir = "${confdir}/conf.d",
+      $logsdir = '/var/log/nginx',
+      $srvname = 'nginx',
+      $srvuser = 'nginx',
+    }
+    'Debian':   {
+      $package = 'nginx',
+      $fowner = 'root',
+      $fgroup = 'root',
+      $docroot = '/var/www',
+      $confdir = '/etc/nginx',
+      $srvblkdir = "${confdir}/conf.d",
+      $logsdir = '/var/log/nginx',
+      $srvname = 'nginx',
+      $srvuser = 'www-data',
+    }
+    'Windows':  {
+      $package = 'nginx-service',
+      $fowner = 'Administrator',
+      $fgroup = 'Administrator',
+      $confdir = 'C:/ProgramData/nginx',
+      $docroot = '${confdir}/html',
+      $srvblkdir = "${confdir}/conf.d",
+      $logsdir = '${confdir}/logs',
+      $srvname = 'nginx',
+      $srvuser = 'nobody',
+    }
+    default:    { fail("OS Family ${osfamily} is not supported) }
+    }
+  }
+  
   File {
-    owner =>  'root',
-    group =>  'root',
+    owner =>  "${fowner}",
+    group =>  "${fgroup}",
     mode  =>  '0644',
   }
 
-  package { 'nginx':
+  package { "$package":
     ensure  =>  present,
   }
 
-  file { "${nginxpath}/nginx.conf":
+  file { "${confdir}/nginx.conf":
     ensure  => file,
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-    notify  =>  Service['nginx'],
+    content =>  template('templates/nginx.conf.erb'),
+    require => Package["${package}"],
+    notify  =>  Service["${srvname}"],
   }
   
-  file { "${nginxpath}/conf.d/defalt.conf":
+  file { "${srvblkdir}/defalt.conf":
     ensure  => file,
-    source  =>  'puppet:///modules/nginx/default.conf',
-    require =>  Package['nginx'],
-    notify  =>  Service['nginx'],
+    content =>  template('templates/default.conf.erb'),
+    require => Package["${package}"],
+    notify  =>  Service["${srvname}"],
   }
   
-  service { 'nginx':
+  service { "$srvname":
     ensure  =>  running,
   }
   
-  file { "${wwwdir}":
+  file { "${docroot}":
     ensure  => directory,
   }
 
-  file { "${wwwdir}/index.html":
+  file { "${docroot}/index.html":
     ensure  => file,
     source  => 'puppet:///modules/nginx/index.html',
-    require =>  File["${wwwdir}"],
+    require =>  File["${docroot}"],
   }
 
 }
