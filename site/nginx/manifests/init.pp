93858b88eb7016ba0d89ab9f988f1cde69d93da5
djs6255 <djs6255@gmail.com>
2016-03-31 08:42:50 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,43 +1,40 @@
 class nginx {
-  $wwwdir = "/var/www"
-  $nginxpath = "/etc/nginx"
-  
+
   case $osfamily {
     'RedHat':   {
-      $package = 'nginx',
-      $fowner = 'root',
-      $fgroup = 'root',
-      $docroot = '/var/www',
-      $confdir = '/etc/nginx',
-      $srvblkdir = "${confdir}/conf.d",
-      $logsdir = '/var/log/nginx',
-      $srvname = 'nginx',
-      $srvuser = 'nginx',
+      $package = 'nginx'
+      $fowner = 'root'
+      $fgroup = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $srvblkdir = "${confdir}/conf.d"
+      $logsdir = '/var/log/nginx'
+      $srvname = 'nginx'
+      $srvuser = 'nginx'
     }
     'Debian':   {
-      $package = 'nginx',
-      $fowner = 'root',
-      $fgroup = 'root',
-      $docroot = '/var/www',
-      $confdir = '/etc/nginx',
-      $srvblkdir = "${confdir}/conf.d",
-      $logsdir = '/var/log/nginx',
-      $srvname = 'nginx',
-      $srvuser = 'www-data',
+      $package = 'nginx'
+      $fowner = 'root'
+      $fgroup = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $srvblkdir = "${confdir}/conf.d"
+      $logsdir = '/var/log/nginx'
+      $srvname = 'nginx'
+      $srvuser = 'www-data'
     }
     'Windows':  {
-      $package = 'nginx-service',
-      $fowner = 'Administrator',
-      $fgroup = 'Administrator',
-      $confdir = 'C:/ProgramData/nginx',
-      $docroot = '${confdir}/html',
-      $srvblkdir = "${confdir}/conf.d",
-      $logsdir = '${confdir}/logs',
-      $srvname = 'nginx',
-      $srvuser = 'nobody',
+      $package = 'nginx-service'
+      $fowner = 'Administrator'
+      $fgroup = 'Administrator'
+      $confdir = 'C:/ProgramData/nginx'
+      $docroot = '${confdir}/html'
+      $srvblkdir = "${confdir}/conf.d"
+      $logsdir = '${confdir}/logs'
+      $srvname = 'nginx'
+      $srvuser = 'nobody'
     }
-    default:    { fail("OS Family ${osfamily} is not supported) }
-    }
+    default:    { fail("OS Family ${osfamily} is not supported") }
   }
   
   File {
