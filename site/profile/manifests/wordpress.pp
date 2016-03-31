2bcaeed88dc0f5ebff61b004a84ff93dbda2d6b9
djs6255 <djs6255@gmail.com>
2016-03-31 14:24:44 -0700

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -2,6 +2,12 @@ class profile::wordpress {
   #mysql server
   
   class { 'mysql' : }
+  mysql::db { 'wordpress':
+    user  =>  'wordpress',
+    password  =>  'wordpress',
+    host  =>  'localhost',
+    grant =>  ['SELECT','UPDATE'],
+  }
   
   #wordpress config
   
@@ -9,10 +15,20 @@ class profile::wordpress {
   class { 'apache' : }
   
   #setup config
-  class { '::wordpress' :  }
+  class { '::wordpress' :
+    wp_owner  =>  'wordpress',
+    wp_group  =>  'wordpress',
+    db_user   =>  'wordpress',
+    db_password =>  'wordpress',
+  }
   
   #local user for wordpress
+  user { 'wordpress':
+    ensure  => present,
+  }
   
   #local grop for wordpress
-  
+  user { 'wordpress':
+    ensure  => 'present'.
+  }
 }
